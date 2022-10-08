80e68275ec14bfe7b48849af8edf4d3ee2da37a5
Registree Rock <88082490+registree-org@users.noreply.github.com>
2022-10-08 13:33:15 +0200

    updated views

--- portfolio/portfolioApp/views.py
+++ portfolio/portfolioApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
-# HomeView function of index()
+
+# Home == function of index()
 def index(request):
     title= 'Welcome to My Portfolio'
     return render(request,'index.html',
@@ -13,8 +17,32 @@ def mapDetails(request):
     context={'title':title})
     
 # Login-user
+def loginView(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data= request.POST)
+        if form.is_valid():
+            username =form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user != None:
+                login(request, user)
+                messages.info(request, f"Logged in as {{username}}")
+                return render(request,'index.html')
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request,"Please kindly check username or password is registered")
+    form = AuthenticationForm()
+    return render (request=request, template_name="admin/")
+
 # Logout-user
+def signUp(request):
+    logout(request)
+    messages.info(request,"You've been successfully logged out")
+    return render(request,'admin/')
+
 # Sign-up user
+
 
 
 
--- portfolio/profiles/views.py
+++ portfolio/profiles/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from models import UserProfile
+from .models import UserProfile
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.db.models.functions import Distance
 
+longitude = {}
+latitude = {}
 # Displaying profiles Details per User_id request
 def profileDetails(request, userProfile_id):
+    model = UserProfile
     profile = get_object_or_404(UserProfile, pk=userProfile_id)
-    user_location = UserProfile.location
+    user_location = profile.location(longitude,latitude, srid=4326)
     return render(request,"profile-page.html",
     context={'profile':profile,'user_location':user_location})
 
