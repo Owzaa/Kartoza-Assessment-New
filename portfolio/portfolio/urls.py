90bec845421985e5c2758a57925b6d8652504bfd
Registree Rock <88082490+registree-org@users.noreply.github.com>
2022-10-11 16:09:14 +0200

    updated url import removed reverse_lazy

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, reverse_lazy
+from django.urls import path
 from portfolioApp.views import homeView,loginView,signOut,signUp,mapDetails
 from profiles.views import profileDetails,editProfile
 urlpatterns = [
